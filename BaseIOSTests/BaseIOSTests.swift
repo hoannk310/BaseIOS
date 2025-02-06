94195e925e3f782228b1434063a6d69964e3a828
hoannk-3066 <nguyen.khai.hoan@sun-asterisk.com>
2025-02-06 14:06:39 +0700

    Initial Commit

---
+++ BaseIOSTests/BaseIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  BaseIOSTests.swift
+//  BaseIOSTests
+//
+//  Created by nguyen.khai.hoan on 6/2/25.
+//
+
+import Testing
+@testable import BaseIOS
+
+struct BaseIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
